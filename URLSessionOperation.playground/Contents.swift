11f74c75302c2ad93e33397be7921876c788056a
Paige Sun <paige.sun@shopify.com>
2017-03-28 10:34:40 -0400

    Add working async Operation subclass.

--- URLSessionOperation.playground/Contents.swift
+++ URLSessionOperation.playground/Contents.swift
@@ -2,12 +2,18 @@
 // https://medium.com/@raulriera/nsoperations-nsoperationqueue-oh-my-88b707f9ba2e#.pvnq1hr3w
 import Foundation
 
+// Need this to make playground wait for the dataTask callback -------------
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
+// --------------------------------------------------------------------------
+
 public typealias URLSessionOperationCompletion = (_ data: Data?, _ response: HTTPURLResponse?, _ error: Error?) -> Void
 
 open class URLSessionOperation: Operation {
 	
 	fileprivate var task: URLSessionDataTask?
-	fileprivate var completionHandler: URLSessionOperationCompletion?
+//	fileprivate var completionHandler: URLSessionOperationCompletion?
 	
 	/**
 	Returns an instance of `URLSessionOperation`
@@ -16,12 +22,12 @@ open class URLSessionOperation: Operation {
 	- parameter url: The URL with the location of the resource to request against.
 	- parameter completion: The block executed iff the request completes.
 	*/
-	public init(session: URLSession = .shared, url: NSURL, completion: @escaping URLSessionOperationCompletion) {
+	public init(session: URLSession = .shared, url: URL, completion: @escaping URLSessionOperationCompletion) {
 		super.init()
 		
 		name = url.absoluteString
-		
-		task = session.dataTask(with: url as URL) { [weak self] (data: Data?, response: URLResponse?, error: Error?) in
+		print("1 setting dataTask for \(name)")
+		task = session.dataTask(with: url) { [weak self] (data: Data?, response: URLResponse?, error: Error?) in
 			self?.completeOperationWithBlock(completion, data: data, response: response as? HTTPURLResponse, error: error)
 		}
 	}
@@ -29,6 +35,7 @@ open class URLSessionOperation: Operation {
 	open override func cancel() {
 		super.cancel()
 		task?.cancel()
+		print("dataTask cancel")
 	}
 	
 	
@@ -37,6 +44,7 @@ open class URLSessionOperation: Operation {
 			//_finished = true
 		} else {
 			//_executing = true
+			print("2 start(): - resuming dataTask")
 			task?.resume()
 		}
 	}
@@ -44,14 +52,32 @@ open class URLSessionOperation: Operation {
 	// MARK: Private
 	
 	fileprivate func completeOperationWithBlock(_ completion: @escaping URLSessionOperationCompletion, data: Data?, response: HTTPURLResponse?, error: Error?) {
-		
+		print("3 completeOperation w block")
 		if isCancelled == false {
 			DispatchQueue.main.async {
 				completion(data, response, error)
+				print(self.isFinished)
 			}
 		}
 		
 		//completeOperation()
+//		completionBlock
 	}
-	
 }
+
+
+let url = URL(string: "https://www.youtube.com")!
+
+let operation = URLSessionOperation(url: url, completion: { _, _ ,_ in print("done 1") })
+let otherOperation = URLSessionOperation(url: url, completion: { _, _ ,_ in print("done 2") })
+otherOperation.addDependency(operation)
+
+
+
+let queue = OperationQueue()
+queue.maxConcurrentOperationCount = 2
+
+queue.addOperation(operation)
+queue.addOperation(otherOperation)
+
+
