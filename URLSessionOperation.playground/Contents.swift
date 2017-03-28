cd36eaa67854ef3a3fe6018783c90c7d43c1d6f7
Paige Sun <paige.sun@shopify.com>
2017-03-28 11:02:54 -0400

    Updated comments.

--- URLSessionOperation.playground/Contents.swift
+++ URLSessionOperation.playground/Contents.swift
@@ -1,4 +1,4 @@
-// Example of an Operation subclass, where the operation runs asynchronously.
+// Wraps an asychrounous network call in an Operation by subclassing Operation.
 // Using Operation & OperationQueue, we can allow only a certain number of concurrent operations,
 // or we can make certain calls dependent on other calls.
 // For example, you can run a network call C, only after network call A & B have finished.
@@ -42,7 +42,6 @@ open class URLSessionOperation: Operation {
 	}
 	
 	open override func start() {
-		
 		if isCancelled {
 			finish()
 		} else {
