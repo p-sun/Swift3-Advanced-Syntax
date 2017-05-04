3f4dd4523699ef098de7c02458e02c8e925b42ee
Paige Sun <paige.sun@shopify.com>
2017-05-04 15:43:34 -0400

    Experiment with NSAttributedString

--- UIControl.playground/Contents.swift
+++ UIControl.playground/Contents.swift
@@ -46,3 +46,27 @@ button2.setTitle("title for control state", for: UIControlState())
 button2.title(for: .normal)
 button2.title(for: .highlighted)
 button2.title(for: .disabled)
+
+
+
+
+// (font, color) -> NSAttributedString
+// Set Attributed string font, weight, and color
+let attributedString = NSAttributedString(string: "a string", attributes: [
+	NSFontAttributeName: UIFont.systemFont(ofSize: 15, weight: 3),
+	NSForegroundColorAttributeName: UIColor.blue
+	])
+// NSAttributedString -> color
+let targetColor = attributedString.attribute(NSForegroundColorAttributeName, at: 0, effectiveRange: nil)
+// NSAttributedString -> font
+let targetFont = attributedString.attribute(NSFontAttributeName, at: 0, effectiveRange: nil)
+// font -> new NSAttributedString
+let newString = NSAttributedString(string: "a new string", attributes: [NSFontAttributeName: targetFont])
+// new NSAttributedString -> the same font, yay!!!!
+let targetFontAgain = newString.attribute(NSFontAttributeName, at: 0, effectiveRange: nil)
+
+
+// I want size and weight back from the NSAttributed String. I can't!
+targetFont.flatMap({ attribute in
+	print(attribute)
+})
